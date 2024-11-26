46fa6ec9766808845b1574b1f8d2dd1667c91199
Indy Iri <indy.iri@student.pxl.be>
2024-11-26 19:13:24 +0100

    WPF aangemaakt

---
+++ H13Oef1Centerparcs/H13Oef1Centerparcs/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace H13Oef1Centerparcs
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
